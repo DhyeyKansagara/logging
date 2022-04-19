329c123b3bca69e1dc7f766ddf23282f6941bac4
Dhyey <dhyeykansagara@gmail.com>
2022-04-19 17:29:20 -0400

    heroku, docker and github setup completed

--- app/formatter/__init__.py
+++ app/formatter/__init__.py
@@ -1,11 +1,15 @@
 import logging
-from flask import request, has_request_context
+from flask import has_request_context,request
 
 class RequestFormatter(logging.Formatter):
     def format(self, record):
         if has_request_context():
             record.url = request.url
             record.remote_addr = request.remote_addr
+            record.request_method = request.method
+            record.request_path = request.path
+            record.ip = request.headers.get('X-Forwarded-For', request.remote_addr)
+            record.host = request.host.split(':', 1)[0]
         else:
             record.url = None
             record.remote_addr = None
