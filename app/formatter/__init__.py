d63e056c4c3da1f92f4f4ae59618d2b0cc59bfe5
Dhyey <dhyeykansagara@gmail.com>
2022-04-19 16:54:01 -0400

    initial commit

---
+++ app/formatter/__init__.py
@@ -0,0 +1,13 @@
+import logging
+from flask import request, has_request_context
+
+class RequestFormatter(logging.Formatter):
+    def format(self, record):
+        if has_request_context():
+            record.url = request.url
+            record.remote_addr = request.remote_addr
+        else:
+            record.url = None
+            record.remote_addr = None
+
+        return super().format(record)
